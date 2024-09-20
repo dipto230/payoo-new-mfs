430fc550b46deec6e49c2fdf1b7213f2e0512ffb
dipto230 <diptoroy230305@gmail.com>
2024-09-20 19:14:41 +0600

    pin verification with event handeler

--- js/home.js
+++ js/home.js
@@ -18,4 +18,23 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
    //get the pin number provided
    const pinNumberInput = document.getElementById('input-filed-number').value;
    console.log(pinNumberInput);
+
+
+   //verify the pin number
+   if(pinNumberInput==='12345'){
+    console.log('adding money to your account');
+   //step-4 add the money
+
+const balance = document.getElementById('account-balance').innerText;
+const addMoneyNumber = parseFloat(addMoneyInput);
+const balanceNumber = parseInt(balance);
+const newBalance = addMoneyNumber + balanceNumber;
+console.log(newBalance);
+
+document.getElementById('account-balance').innerText=newBalance;
+   
+   }
+   else{
+    alert('fail to add the money');
+   }
 });
